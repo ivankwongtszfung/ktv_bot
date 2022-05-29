4fc90051c18595790e6086c3eccc6eb10c0016eb
Ivan Kwong <ivankwongtszfung@gmail.com>
2022-05-28 23:32:11 -0400

    feat(mvxz): get_songs by page

--- ktv_bot/services/mvxz/songs.py
+++ ktv_bot/services/mvxz/songs.py
@@ -46,13 +46,17 @@ class SongService(MvxzRequestor):
     def __init__(self):
         self.url = MVXZ_SEARCH_URL
 
-    def get_songs(self, name: str):
-        html_content = self._fetch(name)
+    def get_songs(self, name: str, page: int):
+        html_content = self._fetch(name, page)
         rows = self._parse(html_content)
         return [Song.from_bs(row) for row in rows]
 
-    def _fetch(self, name: str) -> str:
-        response = requests.post(self.url, headers=self._header(), data={"ktv": name})
+    def _fetch(self, name: str, page: int) -> str:
+        response = requests.post(
+            self.url,
+            headers=self._header(),
+            data={"ktv": name, "page": page, "type": 3},
+        )
         return response.text.encode("latin1")
 
     def _parse(self, html_response: str) -> ResultSet:
--- ktv_bot/tests/unit_tests/mvxz/test_song.py
+++ ktv_bot/tests/unit_tests/mvxz/test_song.py
@@ -18,22 +18,44 @@ def multi_record_html(html_dir):
     return read_html(html_dir / "mvxz_multi_records.html")
 
 
+@fixture
+def multi_no_record_html(html_dir):
+    return read_html(html_dir / "mvxz_no_record.html")
+
+
+@fixture
+def multi_last_plus_one_html(html_dir):
+    return read_html(html_dir / "mvxz_last_plus_one_page.html")
+
+
 class FakeSongService(SongService):
     def __init__(self, data):
         super().__init__()
         self.data = data
 
-    def _fetch(self, _: str) -> str:
+    def _fetch(self, _: str, __: int) -> str:
         return self.data
 
 
 def test_parse_html_content(faker, one_record_html):
     fake_song_service = FakeSongService(data=one_record_html)
-    songs = fake_song_service.get_songs(name=faker.pystr())
+    songs = fake_song_service.get_songs(name=faker.pystr(), page=1)
     assert len(songs) == 1
 
 
 def test_parse_html_content_multiple_records(faker, multi_record_html):
     fake_song_service = FakeSongService(data=multi_record_html)
-    songs = fake_song_service.get_songs(name=faker.pystr())
+    songs = fake_song_service.get_songs(name=faker.pystr(), page=1)
     assert len(songs) == 2
+
+
+def test_parse_html_content_no_records(faker, multi_no_record_html):
+    fake_song_service = FakeSongService(data=multi_no_record_html)
+    songs = fake_song_service.get_songs(name=faker.pystr(), page=1)
+    assert len(songs) == 0
+
+
+def test_parse_html_content_last_plus_one_page(faker, multi_last_plus_one_html):
+    fake_song_service = FakeSongService(data=multi_last_plus_one_html)
+    songs = fake_song_service.get_songs(name=faker.pystr(), page=1)
+    assert len(songs) == 0
