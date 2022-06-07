f9efd0ef108cdf08613de3431524305a449012ef
Ivan Kwong <ivankwongtszfung@gmail.com>
2022-06-07 09:52:10 -0400

    feat: download files in a round robin style

--- e2e/get_songs.py
+++ e2e/get_songs.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+
 import logging
-import os
 import shutil
-import socket
 from collections import deque
+from dataclasses import dataclass
+from functools import cached_property
 from pathlib import Path
 from typing import List, Union
 from urllib.request import urlretrieve
@@ -13,13 +15,14 @@ from tqdm import tqdm
 from ktv_bot.services.ctfile.file import CtFile
 from ktv_bot.services.mvxz.songs import Song, SongService
 
-logging.basicConfig(filename="songs.log", level=logging.DEBUG)
+logging.basicConfig(filename="songs.log", level=logging.INFO)
 
 song_service = SongService()
 ctfile_service = CtFile()
 download_path = Path("/mnt/c/Users/invok/Desktop/KTV/").resolve()
 loaded_paths = [download_path, download_path / "Loaded", download_path / "Loaded 2"]
 temp_path = Path().resolve() / "temp"
+DOWNLOAD_BATCH_SIZE = 5
 
 
 class TqdmUpTo(tqdm):
@@ -68,7 +71,7 @@ def move_file_to_download_folder(tmp_path: Path):
 
 
 def download_all_results(keyword: str, page: int, dfs: bool = True) -> int:
-    """download all songs result
+    """download all songs result for a page
 
     Args:
         keyword (str): search songs with this keyword, must be in simplified chiense
@@ -93,20 +96,73 @@ def download_all_results(keyword: str, page: int, dfs: bool = True) -> int:
     return no_of_songs
 
 
+def download_all_songs(songs: List[Song]) -> int:
+    """download all the songs provided
+
+    Args:
+        songs (List[Song]): songs from mvxz
+
+    Returns:
+        int: number of songs download
+    """
+    no_of_songs = 0
+    for song in songs:
+        try:
+            logging.info(song)
+            download_file(song)
+            no_of_songs += 1
+        except Exception:
+            logging.exception(f"Error occurred while downloading {song.name}")
+            get_tmp_download_filepath(song).unlink(missing_ok=True)
+    return no_of_songs
+
+
 def setup_folder():
     shutil.rmtree(str(temp_path), ignore_errors=True)
     temp_path.mkdir(exist_ok=True)
     download_path.mkdir(exist_ok=True)
 
 
-def main(keywords: List[str], dfs: bool = typer.Option(True, "--dfs/--bfs")):
+@dataclass
+class SongResult:
+    keyword: str
+    page: int = 1
+    current: int = 0
+
+    def get_songs(self, number_of_songs: int):
+        return self.songs[self.current : self.current + number_of_songs]
+
+    @classmethod
+    def from_prev_result(cls, other: SongResult):
+        return cls(other.keyword, other.page + 1)
+
+    @cached_property
+    def songs(self):
+        return song_service.get_songs(self.keyword, self.page)
+
+    @cached_property
+    def length(self):
+        return len(self.songs)
+
+    @property
+    def all_downloaded(self):
+        return self.current == self.length
+
+
+def main(keywords: List[str]):  # , dfs: bool = typer.Option(True, "--dfs/--bfs")):
     setup_folder()
-    song_queue = deque(zip(keywords, [1] * len(keywords)))
-    while song_queue:
-        keyword, page = song_queue.popleft()
-        has_more_songs = download_all_results(keyword, page, dfs)
-        if has_more_songs:
-            song_queue.append((keyword, page + 1))
+    keyword_queue = deque(SongResult(keyword) for keyword in keywords)
+    while keyword_queue:
+        song_result = keyword_queue.popleft()
+        songs = song_result.get_songs(DOWNLOAD_BATCH_SIZE)
+        length = download_all_songs(songs)
+        if length == len(songs):
+            song_result.current += DOWNLOAD_BATCH_SIZE
+        keyword_queue.append(
+            SongResult.from_prev_result(song_result)
+            if song_result.all_downloaded
+            else song_result
+        )
 
 
 if __name__ == "__main__":
